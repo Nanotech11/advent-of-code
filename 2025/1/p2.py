60e084f7eb3c5646e8c2af171e2d84db0f80cdf3
Nanotech11 <nanotech11acc@gmail.com>
2025-11-30 22:50:20 -0700

    even more needless collapsing

--- 2025/1/p2.py
+++ 2025/1/p2.py
@@ -1,12 +1,10 @@
 with open('input.txt', 'r', encoding='utf-8') as file:
-# with open('test_input.txt', 'r', encoding='utf-8') as file:
     rotations = [(line[0], int(line[1:])) for line in file.readlines()]
 
 count = 0
 current_value = 50
 for direction, amount in rotations:
     for i in range(amount):
-        if (current_value := (current_value + (1 if direction == 'R' else -1)) % 100) == 0:
-            count += 1
+        count += int((current_value := (current_value + (1 if direction == 'R' else -1)) % 100) == 0)
 
 print(count)
