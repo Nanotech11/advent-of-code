42dff6c4d52eb2e04eb1179b0c10f8e8122cee54
Nanotech11 <nanotech11acc@gmail.com>
2025-12-01 23:16:56 -0700

    finished 2025-2x

--- 2025/2/p1.py
+++ 2025/2/p1.py
@@ -1,4 +1,14 @@
-# with open('input.txt', 'r', encoding='utf-8') as file:
-with open('test_input.txt', 'r', encoding='utf-8') as file:
-    for line in file.readlines():
+from itertools import chain
 
+with open('input.txt', 'r', encoding='utf-8') as file:
+    ranges = chain.from_iterable(
+        range(int(s), int(e) + 1) for s, e in (group.split('-') for group in file.read().split(','))
+    )
+
+invalids = set()
+for i in ranges:
+    str_i = str(i)
+    if str_i[: len(str_i) // 2] == str_i[len(str_i) // 2 :]:
+        invalids.add(i)
+
+print(sum(invalids))
--- 2025/2/p2.py
+++ 2025/2/p2.py
@@ -1,4 +1,18 @@
-# with open('input.txt', 'r', encoding='utf-8') as file:
-with open('test_input.txt', 'r', encoding='utf-8') as file:
-    for line in file.readlines():
+import re
+from itertools import chain
 
+with open('input.txt', 'r', encoding='utf-8') as file:
+    ranges = chain.from_iterable(
+        range(int(s), int(e) + 1) for s, e in (group.split('-') for group in file.read().split(','))
+    )
+
+invalids = set()
+for i in ranges:
+    str_i = str(i)
+    for j in range(len(str_i) // 2):
+        pattern = re.compile(rf'^({str_i[: j + 1]})+$')
+        if match := pattern.match(str_i):
+            invalids.add(i)
+            break
+
+print(sum(invalids))
