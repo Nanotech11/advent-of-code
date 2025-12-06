fc0749c409629065677a2b16f718dce3b43babe5
Nanotech11 <nanotech11acc@gmail.com>
2025-12-05 22:34:10 -0700

    finished 2025-6x. A bit messy, but quite fast

--- 2025/6/p1.py
+++ 2025/6/p1.py
@@ -1,3 +1,16 @@
-with open(('input.txt', 'test.txt')[1], 'r', encoding='utf-8') as file:
-    lines = [line.strip() for line in file]
+with open(('input.txt', 'test.txt')[0], 'r', encoding='utf-8') as file:
+    lines = [line.strip().split() for line in file]
 
+results = 0
+problems = zip(*lines)
+
+for problem in problems:
+    if problem[-1] == '+':
+        results += sum((int(val) for val in problem[:-1]))
+    else:
+        temp_result = int(problem[0])
+        for val in problem[1:-1]:
+            temp_result *= int(val)
+        results += temp_result
+
+print(results)
---
+++ 2025/6/p2.py
@@ -0,0 +1,32 @@
+from itertools import pairwise
+with open(('input.txt', 'test.txt')[0], 'r', encoding='utf-8') as file:
+    lines = list(file)
+
+operator_line = lines[-1]
+start_indeces = []
+for idx, char in enumerate(operator_line):
+    if char != ' ':
+        start_indeces.append(idx)
+start_indeces.append(len(operator_line) + 1)
+number_lines = []
+for line in lines[:-1]:
+    number_lines.append([line[start:end-1] for start, end in pairwise(start_indeces)])
+
+problems = zip(*number_lines, operator_line.split())
+
+result = 0
+for problem in problems:
+    if problem[-1] == '+':
+        temp_result = 0
+        num_strs = zip(*problem[:-1])
+        for num_str in num_strs:
+            temp_result += int(''.join(num_str))
+        result += temp_result
+    else:
+        num_strs = zip(*problem[:-1])
+        temp_result = int(''.join(next(num_strs)))
+        for num_str in num_strs:
+            temp_result *= int(''.join(num_str))
+        result += temp_result
+
+print(result)
