492684ccc11125f7cf92fe1853b4005554bbc618
Nanotech11 <nanotech11acc@gmail.com>
2025-12-07 00:41:18 -0700

    Refactored to move most of the parsing logic to the read-in. Also making more use of generators and iterators

--- 2025/7/p1.py
+++ 2025/7/p1.py
@@ -1,13 +1,15 @@
-import re
+from itertools import chain
 
 with open(('input.txt', 'test.txt')[0], 'r', encoding='utf-8') as file:
-    lines = [line.strip() for line in file if any(char in line for char in ('S', '^'))]
+    splits = chain.from_iterable(
+        (idx for idx, char in enumerate(line.strip()) if char != '.')
+        for line in file
+        if any(char != '.' for char in line.strip())
+    )
 
-beams = {lines[0].index('S')}
-count = 0
-for line in lines[1:]:
-    indices = (m.start() for m in re.finditer(r'\^', line))
-    for idx in indices:
+    beams = {next(splits)}
+    count = 0
+    for idx in splits:
         if idx in beams:
             count += 1
             beams.remove(idx)
--- 2025/7/p2.py
+++ 2025/7/p2.py
@@ -1,17 +1,19 @@
-import re
+from collections import defaultdict
+from itertools import chain
 
 with open(('input.txt', 'test.txt')[0], 'r', encoding='utf-8') as file:
-    lines = [line.strip() for line in file if any(char in line for char in ('S', '^'))]
+    splits = chain.from_iterable(
+        (idx for idx, char in enumerate(line.strip()) if char != '.')
+        for line in file
+        if any(char != '.' for char in line.strip())
+    )
 
-beams = {i: 0 for i in range(len(lines[0]))}
-beams[lines[0].index('S')] += 1
-timelines = 1
-for line in lines[1:]:
-    indices = (m.start() for m in re.finditer(r'\^', line))
-    for idx in indices:
-        if idx in beams:
-            timelines += beams[idx]
-            beams[idx - 1] += beams[idx]
-            beams[idx + 1] += beams[idx]
-            beams[idx] = 0
+    beams: dict[int, int] = defaultdict(lambda: 0)
+    beams[next(splits)] += 1
+    timelines = 1
+    for idx in splits:
+        timelines += beams[idx]
+        beams[idx - 1] += beams[idx]
+        beams[idx + 1] += beams[idx]
+        beams[idx] = 0
 print(timelines)
